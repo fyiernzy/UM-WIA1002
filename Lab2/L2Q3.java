d812cdd0d99abcc6422ce3d759c923946c87575c
fyiernzy <fyiernzy@gmail.com>
2023-02-03 12:15:28 +0800

    Upload Lab2

---
+++ Lab2/L2Q3.java
@@ -0,0 +1,15 @@
+package Lab2;
+
+public class L2Q3 {
+	public static void main(String[] args) {
+		System.out.println(exponent(10,3));
+	}
+	public static long exponent(int x, int m) {
+		if (m == 0)
+			return 1;
+		if (m == 1)
+			return x;
+		
+		return x * exponent(x, m - 1);
+	}
+}
