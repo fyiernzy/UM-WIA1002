304ba21e62a939040942f44f5c1dea41b5ec1bea
fyiernzy <fyiernzy@gmail.com>
2023-04-13 13:01:15 +0800

    💯 Add .getDevice() method

--- LabTest/LabTest1/Thursday/NumberInterface.java
+++ LabTest/LabTest1/Thursday/NumberInterface.java
@@ -1,8 +1,9 @@
-package LabTest2;
+package LabTest1.Thursday;
 
 public interface NumberInterface<T> {
     public T fromInteger(int value);
     public Integer toInteger();
     public T add(T o);
     public T mul(T o);
+    public String getDevice();
 }
