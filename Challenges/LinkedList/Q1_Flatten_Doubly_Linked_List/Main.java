a02f5977a71860faafc9c0b148e3d811ccc86388
fyiernzy <55268504+fyiernzy@users.noreply.github.com>
2023-04-19 13:35:59 +0800

    ✨ Update Main.java

--- Challenges/LinkedList/Q1_Flatten_Doubly_Linked_List/Main.java
+++ Challenges/LinkedList/Q1_Flatten_Doubly_Linked_List/Main.java
@@ -1,11 +1,10 @@
-package Scratch;
+package Challenges.LinkedList.Q1;
 
 public class Main {
     public static void main(String[] args) {
         Node head;
         String format = "%20s : %s\n";
-
-
+        
         head = NodeUtil.toNodeList("[1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]");
         System.out.printf(format, "Input", NodeUtil.toString(head));
         NodeUtil.flattenIterative(head);
